469a2ebd6d95d46329def83157473973201d1601
Vladislav Tankov <vdtankov@gmail.com>
2016-07-30 00:25:29 +0300

    Splitting services 1.Optimization of Travis configuration

--- buildAppSplitted.sh
+++ buildAppSplitted.sh
@@ -5,11 +5,11 @@ sh thriftGenSplitted.sh
 cd DBServices
 #build DiagramDBService
 cd DiagramService
-mvn clean install
+mvn clean package
 #build RobotsDBService
 cd ../RobotsService
-mvn clean install
+mvn clean package
 #build UserDbService
 cd ../UserService
-mvn clean install
+mvn clean package
 echo "Now use tomcat7 to start project"
