e244deb93d578c37dde931be819a54afb0e60dba
Timofey Bryksin <timofey.bryksin@gmail.com>
2016-08-01 00:57:11 +0300

    fixed some codacy and IDEA warnings

--- SharedResources/src/main/webapp/resources/js/transform-to-element-fix.js
+++ SharedResources/src/main/webapp/resources/js/transform-to-element-fix.js
@@ -1,7 +1,3 @@
-/**
- * Created by vladimir-zakharov on 27.02.16.
- */
-
 SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformToElement || function(elem) {
         return elem.getScreenCTM().inverse().multiply(this.getScreenCTM());
     };
