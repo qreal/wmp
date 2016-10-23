b47a85b21cd417837babf10be7e997d01e105486
Artemii Bezguzikov <artemii.bezguzikov@gmail.com>
2016-10-23 18:06:00 +0300

    Add travis support for GUI testing
    1. Add .property file for UI-testing in oder to specify profiles
    2. Modify .travis.yml file so that it includes GUI testing

--- UI-testing/src/test/java/com/qreal/wmp/uitesting/auth/AuthDashboardTest.java
+++ UI-testing/src/test/java/com/qreal/wmp/uitesting/auth/AuthDashboardTest.java
@@ -1,23 +1,48 @@
 package com.qreal.wmp.uitesting.auth;
 
+import com.codeborne.selenide.WebDriverRunner;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 
 public class AuthDashboardTest {
 
+    private static String dashboardUrl;
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setUpClass() {
+        ChromeDriverManager.getInstance().setup();
+        final String resourceName = "services.properties";
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Properties props = new Properties();
+        try(InputStream resourceStream = loader.getResourceAsStream(resourceName)) {
+            props.load(resourceStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        dashboardUrl = props.getProperty("accessDashboardUri");
+    }
+
     @Before
     public void openAuthPage() {
-        open("http://localhost:8080/dashboard/");
+        driver = new ChromeDriver();
+        WebDriverRunner.setWebDriver(driver);
+        open(dashboardUrl);
         $(byText("Sign in to continue to Auth")).shouldBe(exist);
         $(byText("Dashboard")).shouldNotBe(exist);
     }
@@ -27,14 +52,15 @@ public class AuthDashboardTest {
         $(By.name("username")).setValue("123");
         $(By.name("password")).setValue("123");
         $("[type=\"submit\"]").click();
-        $(byText("Dashboard")).waitUntil(appear, 10000);
+        $(byText("Dashboard")).waitUntil(appear, 50000);
     }
 
     @Test
     public void  userWrongAuth() {
         $(byText("Password or login wrong")).shouldNotBe(exist);
-        String wrongLogin = RandomStringUtils.random(20);
-        String wrongPassword = RandomStringUtils.random(20);
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+        String wrongLogin = RandomStringUtils.random(20, alphabet);
+        String wrongPassword = RandomStringUtils.random(20, alphabet);
         $(By.name("username")).setValue(wrongLogin);
         $(By.name("password")).setValue(wrongPassword);
         $("[type=\"submit\"]").click();
@@ -43,8 +69,7 @@ public class AuthDashboardTest {
 
     @After
     public void logout() {
-
-        // Should logout, but now simply close the browser
-        close();
+       driver.close();
     }
+
 }
